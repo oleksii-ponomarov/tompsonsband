eb103826acd0df2761e3ac181a3473c6faa38a45
oleksii-ponomarov <rocknrollerr@gmail.com>
2021-11-27 10:11:16 +0200

    optimized email script

---
+++ assets/js/email.js
@@ -0,0 +1,31 @@
+function sendInfo(name, phone, info) {
+  const form = document.getElementById("form");
+  const sent = document.getElementById("sent");
+
+  if (name && phone && info) {
+    console.log(name, phone, info);
+    form.style.transform = "translateX(100vw)";
+    sent.style.transform = "translateX(0)";
+
+    var template_params = {
+      name: name,
+      phone: phone,
+      message: info,
+    };
+
+    var service_id = "default_service";
+    var template_id = "request";
+    emailjs.send(service_id, template_id, template_params);
+  }
+}
+
+function handleSend() {
+  sendInfo(
+    document.getElementById("name").value,
+    document.getElementById("phone").value,
+    document.getElementById("info").value
+  );
+}
+
+const sendButton = document.getElementById("send");
+sendButton.addEventListener("click", handleSend);
